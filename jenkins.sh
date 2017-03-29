06317d88d8a5680ca8a9e6dc59203f6c749e582f
Kambiz Darabi <darabi@m-creations.net>
2017-03-29 11:15:08 +0200

    Add JAVA_HOME to PATH in jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,7 @@
 #! /bin/bash -e
 
+export PATH=${JAVA_HOME}/bin:$PATH
+
 : "${JENKINS_HOME:="/var/jenkins_home"}"
 touch "${COPY_REFERENCE_FILE_LOG}" || { echo "Can not write to ${COPY_REFERENCE_FILE_LOG}. Wrong volume permissions?"; exit 1; }
 echo "--- Copying files at $(date)" >> "$COPY_REFERENCE_FILE_LOG"
